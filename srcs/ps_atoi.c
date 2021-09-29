8615d2f0f92533aeb538775c012fbb9a07fb5129
Sylsee <sylvio.poliart@gmail.com>
2021-09-29 21:10:56 +0200

    fix norme on ps_atoi

--- srcs/ps_atoi.c
+++ srcs/ps_atoi.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 19:45:13 by spoliart          #+#    #+#             */
-/*   Updated: 2021/09/25 03:05:01 by marvin           ###   ########.fr       */
+/*   Updated: 2021/09/29 21:10:04 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ps_atoi(char *s, int *err)
 	{
 		nbr = (nbr * 10) + (*s++ - '0');
 		if ((sign == 1 && nbr > INT_MAX) || (sign == -1
-			&& nbr > (long)(INT_MAX) + 1))
+				&& nbr > (long)(INT_MAX) + 1))
 		{
 			*err = 1;
 			return (0);
